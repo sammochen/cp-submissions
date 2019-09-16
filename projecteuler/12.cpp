a05ca783e18f9dbe7e70f1dd3a81eed6ca7ddd1b
Sam <samuelshchen@gmail.com>
2019-09-16 16:49:45 +1200

    update 16-09-19

---
+++ projecteuler/12.cpp
@@ -0,0 +1,73 @@
+/*
+Original problem:
+Author: sammochen
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l;x<u;x++)
+#define RREP(x,l,u) for(ll x = l;x>=u;x--)
+
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+
+string to_string(char c) {return "" + c;}
+string to_string(bool b) {return b ? "true" : "false";}
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+
+namespace SOLVE {
+	ll d(ll x) {
+		ll ans = 0;
+		for (ll f = 1; f * f <= x; f++) {
+			if (x % f == 0) {
+				ans += f * f == x ? 1 : 2;
+			}
+		}
+		return ans;
+	}
+	
+	ll f(ll x) {
+		return (x * (x + 1)) / 2;
+	}
+	
+	void main() {
+		ll n = 500;
+		ll x = 1;
+		while (1) {
+			if (d(f(x)) > 500) {
+				cout << f(x) << endl;
+				return;
+			}
+			x++;
+		}
+		
+	}
+}
+
+
+signed main() {
+	int t;
+	t = 1;
+	// cin >> t;
+	while (t--) {
+		SOLVE::main();
+	}
+
+	return 0;
+}
