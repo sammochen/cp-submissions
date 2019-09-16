b0b3b1a8a3dbc1fef25ebd484d39644508a6b0ed
Sam <samuelshchen@gmail.com>
2019-09-16 12:46:43 +1200

    update 16-09-19

---
+++ projecteuler/1.cpp
@@ -0,0 +1,70 @@
+/*
+Original problem:
+Author: sammochen
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l;x<u;x++)
+#define RREP(x,l,u) for(ll x = l;x>=u;x--)
+
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+
+string to_string(char c) {return "" + c;}
+string to_string(bool b) {return b ? "true" : "false";}
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+
+namespace SOLVE {
+	ll t(ll x) {
+		return (x * (x+1)) / 2;
+	}
+	
+	ll sum(ll n, ll a) {
+		return t(n / a) * a;
+	}
+	
+	ll gcd(ll a, ll b) {
+		return b == 0 ? a : gcd(b, a%b);
+	}
+	
+	ll lcm(ll a, ll b) {
+		return a * b / gcd(a,b);
+	}
+	
+	void main() {
+		ll n, a, b;
+		cin >> n >> a >> b;
+		ll ans = sum(n - 1, a) + sum(n - 1, b) - sum(n - 1, lcm(a,b));
+		
+		cout << ans << endl;
+	}
+}
+
+
+signed main() {
+	int t;
+	t = 1;
+	// cin >> t;
+	while (t--) {
+		SOLVE::main();
+	}
+
+	return 0;
+}
