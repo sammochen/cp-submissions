a2fe26ab38584da6dc2633b8bcf52bcc51ca8ac4
Sam <samuelshchen@gmail.com>
2019-09-16 17:45:04 +1200

    update 16-09-19

---
+++ projecteuler/15.cpp
@@ -0,0 +1,59 @@
+/*
+Original problem:
+Author: sammochen
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l;x<u;x++)
+#define RREP(x,l,u) for(ll x = l;x>=u;x--)
+
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+
+string to_string(char c) {return "" + c;}
+string to_string(bool b) {return b ? "true" : "false";}
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+
+namespace SOLVE {
+	void main() {
+		ll n = 20;
+		vector<VLL> dp(n+1,VLL(n+1));
+		dp[0][0] = 1;
+		REP(i,0,n+1) {
+			REP(j,0,n+1) {
+				if (i != 0) dp[i][j] += dp[i-1][j];
+				if (j != 0) dp[i][j] += dp[i][j-1];
+			}
+		}
+		cout << dp[20][20] << endl;
+	}
+}
+
+
+signed main() {
+	int t;
+	t = 1;
+	// cin >> t;
+	while (t--) {
+		SOLVE::main();
+	}
+
+	return 0;
+}
