e1156966a7d6253317cbe761138795d712faea04
Sam <samuelshchen@gmail.com>
2019-09-17 21:43:22 +1200

    update 17-09-19

---
+++ projecteuler/45.cpp
@@ -0,0 +1,64 @@
+/*
+Original problem:
+Author: sammochen
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l;x<u;x++)
+#define RREP(x,l,u) for(ll x = l;x>=u;x--)
+
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+
+string to_string(string s) {return s;}
+string to_string(char c) {return "" + c;}
+string to_string(bool b) {return b ? "true" : "false";}
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+
+namespace SOLVE {	
+	void main() {
+		map<ll,ll> f;
+		ll n = 100000;
+		REP(i,0,n) {
+			ll t = (i * (i+1)) / 2;
+			ll p = (i * (3*i - 1)) / 2;
+			ll h = i * (2*i - 1);
+			
+			if (++f[t] == 3) debug(t);
+			if (++f[p] == 3) debug(t);
+			if (++f[h] == 3) debug(t);
+			
+		}
+		
+	}
+}
+
+
+signed main() {
+	int t;
+	t = 1;
+	// cin >> t;
+	while (t--) {
+		SOLVE::main();
+	}
+
+	return 0;
+}
