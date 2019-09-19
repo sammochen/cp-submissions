da49dd6de766df156d0910ae32d9024ba6019b34
Sam <samuelshchen@gmail.com>
2019-09-19 18:59:13 +1200

    added 301

---
+++ projecteuler/301.cpp
@@ -0,0 +1,70 @@
+/*
+Original problem:
+Author: sammochen
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l;x<u;x++)
+#define RREP(x,l,u) for(ll x = l;x>=u;x--)
+
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+#define MP make_pair
+
+string to_string(string s) {return s;}
+string to_string(char c) {return "" + c;}
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+
+namespace SOLVE {	
+	void main() {
+		// no consecutive 1s! dp approach 
+		// eg 100101 is valid, but 101100 is not
+		
+		ll n = 30;
+		
+		VLL dp(2,1); // dp[i] is the number of ways ending at i
+		
+		REP(i,1,n) {
+			VLL next(2,0);
+			next[0] = dp[0] + dp[1];
+			next[1] = dp[0];
+			
+			dp = next;
+		}
+		
+		ll ans = dp[0] + dp[1];
+		cout << ans << endl;
+		
+		
+	}
+}
+
+
+signed main() {
+	int t;
+	t = 1;
+	// cin >> t;
+	while (t--) {
+		SOLVE::main();
+	}
+
+	return 0;
+}
