4095e872a3bb1c5709e14eb4c22cb00925bcf20d
Sam <samuelshchen@gmail.com>
2019-09-23 10:12:22 +1200

    added 1220B-1300-multiplication-table

---
+++ codeforces/1220B-1300-multiplication-table.cpp
@@ -0,0 +1,72 @@
+/*
+Original problem:
+Author: sammochen
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l;x<u;x++)
+#define RREP(x,l,u) for(ll x = l;x>=u;x--)
+
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+#define MP make_pair
+
+string to_string(string s) {return s;}
+string to_string(char c) {return "" + c;}
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+
+namespace SOLVE {	
+	void main() {
+		ll n;
+		cin >> n;
+		
+		vector<VLL> A(n, VLL(n));
+		
+		REP(i,0,n) {
+			REP(j,0,n) {
+				cin >> A[i][j];
+			}
+		}
+		
+		REP(i,0,n) {
+			ll a = (i+1)%n, b = (i+2)%n;
+			ll ans = sqrt(A[i][a] * A[i][b] / A[a][b]);
+			cout << ans << ' ';
+		}
+		cout << endl;
+	}
+}
+
+
+signed main() {
+	ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+	int t;
+	t = 1;
+	// cin >> t;
+	while (t--) {
+		SOLVE::main();
+	}
+
+	return 0;
+}
