627a2ba17cd1f7f827f58a2b30b5cf001c3c41a5
sammochen <samuelshchen@gmail.com>
2019-09-24 16:55:38 +1200

    added max-even-subarray

---
+++ csacademy/max-even-subarray.cpp
@@ -0,0 +1,76 @@
+/*
+Original problem:
+Author: sammochen
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l;x<u;x++)
+#define RREP(x,l,u) for(ll x = l;x>=u;x--)
+
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+#define MP make_pair
+
+string to_string(string s) {return s;}
+string to_string(char c) {return "" + c;}
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+
+ll const inf = (ll)1e18 + 5;
+
+namespace SOLVE {	
+	void main() {
+		ll n;
+		cin >> n;
+		// create 2 dp, one is only allowed to reset from 0/1 parity
+		VLL dp(2, -inf);
+		
+		ll x;
+		ll ans = -inf;
+		
+		REP(i,0,n) {
+			cin >> x;
+			
+			VLL next(2);
+			
+			
+			next[0] = max(dp[1] + x, x);
+			next[1] = dp[0] + x;	
+			
+			dp = next;
+			ans = max(ans, dp[1]);
+			// debug(i, dp);
+		}
+		
+		cout << ans << endl;
+	}
+}
+
+
+signed main() {
+	int t;
+	t = 1;
+	// cin >> t;
+	while (t--) {
+		SOLVE::main();
+	}
+
+	return 0;
+}
