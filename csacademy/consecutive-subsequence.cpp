e2681846b466c74cd4c532ca71f7038dab274dc7
sammochen <samuelshchen@gmail.com>
2019-09-24 20:35:02 +1200

    added consecutive-subsequence

---
+++ csacademy/consecutive-subsequence.cpp
@@ -0,0 +1,66 @@
+/*
+Original problem:
+Author: sammochen
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l;x<u;x++)
+#define RREP(x,l,u) for(ll x = l;x>=u;x--)
+
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+#define MP make_pair
+
+string to_string(string s) {return s;}
+string to_string(char c) {return "" + c;}
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+
+namespace SOLVE {	
+	void main() {
+		ll n, x;
+		cin >> n;
+		
+		map<ll,ll> og;
+		map<ll,ll> add;
+		
+		ll ans = 0;
+		REP(i,0,n) {
+			cin >> x;
+			og[x] = og[x-1] + 1;
+			add[x] = max(add[x-1] + 1, og[x-2] + 2);
+			
+			ans = max({ans, og[x], add[x]});
+		}
+		cout << ans << endl;
+	}
+}
+
+
+signed main() {
+	int t;
+	t = 1;
+	// cin >> t;
+	while (t--) {
+		SOLVE::main();
+	}
+
+	return 0;
+}
