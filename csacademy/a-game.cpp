aa7f8fce2cd77a156186af38cebb2493f8e0c40e
sammochen <samuelshchen@gmail.com>
2019-09-27 18:06:51 +1200

    added a-game

---
+++ csacademy/a-game.cpp
@@ -0,0 +1,78 @@
+/*
+Original problem:
+Author: sammochen
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l;x<u;x++)
+#define RREP(x,l,u) for(ll x = l;x>=u;x--)
+
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+#define MP make_pair
+
+string to_string(string s) {return s;}
+string to_string(char c) {return "" + c;}
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+
+namespace SOLVE {	
+	void main() {
+		ll n;
+		cin >> n;
+		string s;
+		cin >> s;
+		
+		ll acount = 0;
+		ll streak = 0;
+		ll ans = 0;
+		
+		REP(i,0,n) {
+			if (s[i] == 'A') {
+				acount++;
+				ans ^= streak;
+				streak = 0;
+			} else {
+				streak++;
+			}
+		}
+		
+		ans ^= streak;
+		
+		if (acount % 2) {
+			if (ans == 0) cout << 'B' << endl;
+			else cout << 'A' << endl;
+		} else {
+			cout << -1 << endl;
+		}
+	}
+}
+
+
+signed main() {
+	int t;
+	t = 1;
+	// cin >> t;
+	while (t--) {
+		SOLVE::main();
+	}
+
+	return 0;
+}
