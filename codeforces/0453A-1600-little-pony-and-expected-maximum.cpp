c6d0f703728319eca639769765ccd5ea21752139
sammochen <samuelshchen@gmail.com>
2019-09-30 11:41:50 +1300

    added 0453A-1600-little-pony-and-expected-maximum

---
+++ codeforces/0453A-1600-little-pony-and-expected-maximum.cpp
@@ -0,0 +1,63 @@
+/*
+Original problem:
+Author: sammochen
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l;x<u;x++)
+#define RREP(x,l,u) for(ll x = l;x>=u;x--)
+
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+#define MP make_pair
+
+string to_string(string s) {return s;}
+string to_string(char c) {return "" + c;}
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+
+namespace SOLVE {	
+	void main() {
+		ll m, n;
+		cin >> m >> n;
+		// m dice, n times
+		vector<db> p(m+1); // p[i] is the change with i max
+		REP(i,0,m+1) {
+			p[i] = pow(((db)i / m), n);
+		}
+		db ans = 0;
+		REP(i,1,m+1) {
+			ans += i * (p[i] - p[i-1]);
+		}
+		cout << setprecision(20) << ans << endl;
+	}
+}
+
+
+signed main() {
+	int t;
+	t = 1;
+	// cin >> t;
+	while (t--) {
+		SOLVE::main();
+	}
+
+	return 0;
+}
