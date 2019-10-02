82473028d882a5cd01b6a1b2734cd297ec016a12
sammochen <samuelshchen@gmail.com>
2019-10-03 08:09:01 +1300

    added 0069A-1100-young-physicist

---
+++ codeforces/0069A-1100-young-physicist.cpp
@@ -0,0 +1,68 @@
+/*
+Original problem:
+Author: sammochen
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l;x<u;x++)
+#define RREP(x,l,u) for(ll x = l;x>=u;x--)
+
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+#define MP make_pair
+
+string to_string(string s) {return s;}
+string to_string(char c) {return "" + c;}
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+
+namespace SOLVE {	
+	void main() {
+		ll n;
+		cin >> n;
+		VLL s(3);
+		REP(i,0,n) {
+			VLL x(3);
+			cin >> x[0] >> x[1] >> x[2];
+			REP(j,0,3) {
+				s[j] += x[j];
+			}
+		}
+
+		ll ok = 1;
+		REP(i,0,3) {
+			if (s[i]) ok = 0;
+		}
+		string ans = ok ? "YES" : "NO";
+		cout << ans << endl;
+	}
+}
+
+
+signed main() {
+	int t;
+	t = 1;
+	// cin >> t;
+	while (t--) {
+		SOLVE::main();
+	}
+
+	return 0;
+}
