8001c0e24e4def5a9f75d90842daf9f46b01933c
sammochen <samuelshchen@gmail.com>
2019-10-06 21:49:36 +1300

    added 1084C-1500-the-fair-nut-and-the-string

---
+++ codeforces/1084C-1500-the-fair-nut-and-the-string.cpp
@@ -0,0 +1,72 @@
+/*
+Original problem:
+Author: sammochen
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l;x<u;x++)
+#define RREP(x,l,u) for(ll x = l;x>=u;x--)
+
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+#define MP make_pair
+
+string to_string(string s) {return s;}
+string to_string(char c) {return "" + c;}
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+template <typename A, typename B> string to_string(pair<A,B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+
+const ll mod = 1e9+7;
+
+namespace SOLVE {	
+	void main() {
+		string s;
+		cin >> s;
+		s = "b" + s + "b";
+		
+		ll ans = 0;
+		ll streak = 0;
+		for (char c : s) {
+			if (c == 'b') {
+				ans += (ans + 1) * streak % mod;
+				ans %= mod;
+				streak = 0;
+			} else if (c == 'a') {
+				streak++;
+			}
+		}
+
+		cout << ans << endl;
+
+	}
+}
+
+
+signed main() {
+	int t;
+	t = 1;
+	// cin >> t;
+	while (t--) {
+		SOLVE::main();
+	}
+
+	return 0;
+}
