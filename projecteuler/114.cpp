4460cd8224ce2827221792c65113239fd504efa8
sammochen <samuelshchen@gmail.com>
2019-11-05 14:22:43 +1300

    added 114

---
+++ projecteuler/114.cpp
@@ -0,0 +1,73 @@
+/*
+Original problem:
+Author: sammochen
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l;x<u;x++)
+#define RREP(x,l,u) for(ll x = l;x>=u;x--)
+
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+#define MP make_pair
+
+string to_string(string s) {return s;}
+string to_string(char c) {return "" + c;}
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+template <typename A, typename B> string to_string(pair<A,B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+
+namespace SOLVE {	
+	void main() {
+		// dp[i] is the number of ways when a red ends at i
+		ll n, k = 3;
+		cin >> n;
+
+		VLL dp(n+1); // number of ways ending here
+
+
+		REP(i,1,n+1) {
+			dp[i] = max(0LL, i-k+1);
+
+			REP(j,1,i-k) {
+				dp[i] += dp[j] * (i - j - k);
+			}
+		}
+
+		ll ans = 0 ;
+		REP(i,0,n+1) ans += dp[i];
+		ans += 1; // empty
+		cout << ans << endl;
+
+
+	}
+}
+
+
+signed main() {
+	int t;
+	t = 1;
+	// cin >> t;
+	while (t--) {
+		SOLVE::main();
+	}
+
+	return 0;
+}
