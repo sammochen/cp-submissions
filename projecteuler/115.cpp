abbe13e62460b5bc6a3de580e0731ae463a06fbd
sammochen <samuelshchen@gmail.com>
2019-11-05 14:28:51 +1300

    added 115

---
+++ projecteuler/115.cpp
@@ -0,0 +1,81 @@
+/*
+Original problem:
+Author: sammochen
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l;x<u;x++)
+#define RREP(x,l,u) for(ll x = l;x>=u;x--)
+
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+#define MP mame_pair
+
+string to_string(string s) {return s;}
+string to_string(char c) {return "" + c;}
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+template <typename A, typename B> string to_string(pair<A,B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+
+namespace SOLVE {	
+	void main() {
+		ll n = 10000; // some arbitrarily large number
+		ll m = 50; 
+		
+
+		VLL dp(n+1); // number of ways ending here
+		VLL pre(n+1); // pre[i] is the total up to i
+		pre[0] = 1; // empty is 1
+
+		ll ans;
+		REP(i,1,n+1) {
+			dp[i] = max(0LL, i-m+1);
+
+			REP(j,1,i-m) {
+				dp[i] += dp[j] * (i - j - m);
+			}
+
+			pre[i] = pre[i-1] + dp[i];
+
+			if (pre[i] > 1000000) {
+				ans = i;
+				break;
+			}
+		}
+
+		cout << ans << endl;
+
+
+
+
+	}
+}
+
+
+signed main() {
+	int t;
+	t = 1;
+	// cin >> t;
+	while (t--) {
+		SOLVE::main();
+	}
+
+	return 0;
+}
