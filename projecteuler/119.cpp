ef0dbe231ca422a1bbf7903ad0b84daccf52bdc2
sammochen <samuelshchen@gmail.com>
2019-11-07 08:05:12 +1300

    added 119

---
+++ projecteuler/119.cpp
@@ -0,0 +1,77 @@
+/*
+Original problem:
+Author: sammochen
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l;x<u;x++)
+#define RREP(x,l,u) for(ll x = l;x>=u;x--)
+
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+#define MP make_pair
+
+string to_string(string s) {return s;}
+string to_string(char c) {return "" + c;}
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+template <typename A, typename B> string to_string(pair<A,B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+
+namespace SOLVE {	
+	ll sum(ll x) {
+		if (x < 10) return -1; // if less than 10, it has no sum
+		string s = to_string(x);
+		ll ans = 0;
+		for (char c : s) ans += c - '0';
+		return ans;
+	}
+
+	void main() {
+		ll nax = 1e16; // set an upper bound on how much we care
+		ll basen = 500; // random guess
+		VLL nums;
+		REP(base,2,basen) {
+			REP(power,1,basen) {
+				ll x = pow(base,power);
+				if (x >= nax) break;
+				if (sum(x) == base) nums.push_back(x);
+				
+			}
+		}
+
+		sort(nums.begin(), nums.end());
+		debug(nums, nums.size());
+
+		ll ans = nums[29];
+		cout << ans << endl;
+	}
+}
+
+
+signed main() {
+	int t;
+	t = 1;
+	// cin >> t;
+	while (t--) {
+		SOLVE::main();
+	}
+
+	return 0;
+}
