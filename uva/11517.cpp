2a17301ac029d50e64302e2980b18488c7ff51fd
sammochen <samuelshchen@gmail.com>
2019-11-09 17:43:57 +1300

    added 11517

---
+++ uva/11517.cpp
@@ -0,0 +1,78 @@
+/*
+Original problem:
+Author: sammochen
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l;x<u;x++)
+#define RREP(x,l,u) for(ll x = l;x>=u;x--)
+
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+#define MP make_pair
+
+string to_string(string s) {return s;}
+string to_string(char c) {return "" + c;}
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+template <typename A, typename B> string to_string(pair<A,B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+
+const ll inf = 1e9+7;
+
+namespace SOLVE {	
+	
+	void main() {
+		ll k, n;
+		ll nax = 20005;
+		cin >> k >> n;
+
+		VLL coins(n);
+
+		REP(i,0,n) cin >> coins[i];
+		
+		VLL dp(nax, inf); // dp[coin index][total] = least amount of coins used
+
+		dp[0] = 0;
+		REP(i,0,n) {
+			RREP(j,nax-1,0) { // go backwards
+				if (j + coins[i] < nax) dp[j+coins[i]] = min(dp[j+coins[i]], dp[j] + 1);
+			}
+		}
+
+		REP(money, k, nax) {
+			if (dp[money] != inf) {
+				cout << money << ' ' << dp[money] << endl;
+				return;
+			}
+		}
+	}
+}
+
+
+signed main() {
+	int t;
+	t = 1;
+	cin >> t;
+	while (t--) {
+		SOLVE::main();
+	}
+
+	return 0;
+}
