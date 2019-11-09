1486f44924997ba9b093d53b4adb6aa01101a1ef
sammochen <samuelshchen@gmail.com>
2019-11-09 23:52:41 +1300

    added 00562

---
+++ uva/00562.cpp
@@ -0,0 +1,78 @@
+/*
+Original problem:
+Author: sammochen
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l;x<u;x++)
+#define RREP(x,l,u) for(ll x = l;x>=u;x--)
+
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+#define MP make_pair
+
+string to_string(string s) {return s;}
+string to_string(char c) {return "" + c;}
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+template <typename A, typename B> string to_string(pair<A,B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+
+namespace SOLVE {	
+	void main() {
+		ll n, nax = 10001, total = 0;
+		cin >> n;
+		VLL A(n);
+
+		REP(i,0,n) {
+			cin >> A[i];
+			total += A[i];
+		}
+
+		VLL possible(total + 1);
+		possible[0] = 1;
+		REP(i,0,n) {
+			RREP(j,total,0) {
+				if (j + A[i] <= total) possible[j + A[i]] |= possible[j];
+			}
+		} 
+
+		ll ans = nax;
+		REP(i,0,total + 1) {
+			if (possible[i]) {
+				ans = min(ans, abs(i - (total - i)));
+			}
+		}
+
+		cout << ans << endl;
+
+	}
+}
+
+
+signed main() {
+	int t;
+	// t = 1;
+	cin >> t;
+	while (t--) {
+		SOLVE::main();
+	}
+
+	return 0;
+}
