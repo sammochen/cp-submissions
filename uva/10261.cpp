217b95acc9bbb46c5943f8960a340e787be85243
sammochen <samuelshchen@gmail.com>
2019-11-16 21:39:25 +1300

    added 10261

---
+++ uva/10261.cpp
@@ -0,0 +1,108 @@
+/*
+Original problem:
+Author: sammochen
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l;x<u;x++)
+#define RREP(x,l,u) for(ll x = l;x>=u;x--)
+
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+#define MP make_pair
+
+string to_string(string s) {return s;}
+string to_string(char c) {return "" + c;}
+template <typename A, typename B> string to_string(pair<A,B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+
+const ll inf = (ll)1e18 + 5;
+
+namespace SOLVE {	
+	VVLL dp;
+	VLL pre;
+	ll n;
+	vector<string> ans;
+
+	ll get(VLL & A, ll a, ll ind) {
+		if (a > n || pre[ind] - a > n) return -inf;
+		if (ind == A.size()) return 0;
+		if (dp[a][ind] != -1) return dp[a][ind];
+
+		dp[a][ind] = max({get(A, a, ind + 1) + A[ind], get(A, a + A[ind], ind + 1) + A[ind], 0LL});
+		return dp[a][ind];
+	}
+
+	void printans(VLL & A, ll a, ll ind) {
+		if (ind == A.size()) return;
+
+		if (get(A, a, ind) == get(A, a + A[ind], ind + 1) + A[ind]) {
+			ans.push_back("port");
+			printans(A, a + A[ind], ind + 1);
+		} else if (get(A, a, ind) == get(A, a, ind + 1) + A[ind]) {
+			ans.push_back("starboard");
+			printans(A, a, ind + 1);
+		}
+	}
+
+	void main() {
+		VLL A;
+		cin >> n;
+		n *= 100;
+
+		while (1) {
+			ll x;
+			cin >> x;
+			if (x == 0) break;
+			A.push_back(x);
+		}
+
+		dp.clear();
+		dp.resize(n + 1, VLL(A.size(), -1));
+
+		pre.clear();
+		pre.resize(A.size() + 1);
+		REP(i,0,A.size()) pre[i+1] = pre[i] + A[i];
+
+		ans.clear();
+		printans(A, 0, 0);
+
+		cout << ans.size() << endl;
+		for (string s : ans) cout << s << endl;
+	}
+}
+
+
+signed main() {
+	ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+	
+	int t;
+	// t = 1;
+	cin >> t;
+	while (t--) {
+		SOLVE::main();
+		if (t != 0) cout << endl;
+	}
+
+	return 0;
+}
