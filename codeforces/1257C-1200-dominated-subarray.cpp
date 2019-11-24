892d74cde80e81cac2ed47cc713465f6e1b09e77
sammochen <samuelshchen@gmail.com>
2019-11-24 15:27:37 +1300

    added 1257C-1200-dominated-subarray

---
+++ codeforces/1257C-1200-dominated-subarray.cpp
@@ -0,0 +1,75 @@
+/*
+Original problem:
+Author: sammochen
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l;x<u;x++)
+#define RREP(x,l,u) for(ll x = l;x>=u;x--)
+
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+#define MP make_pair
+
+string to_string(string s) {return s;}
+string to_string(char c) {return "" + c;}
+template <typename A, typename B> string to_string(pair<A,B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+
+const ll inf = 1e9 + 7;
+
+namespace SOLVE {	
+	void main() {
+		ll n;
+		cin >> n;
+		VLL A(n);
+		REP(i,0,n) cin >> A[i];
+
+		VLL last(n+1, -1);
+		ll ans = inf;
+		REP(i,0,n) {
+			if (last[A[i]] != -1) {
+				ans = min(ans, i - last[A[i]] + 1);
+			}
+
+			last[A[i]] = i;
+		}
+
+		ans = ans == inf ? -1 : ans;
+		cout << ans << endl;
+	}
+}
+
+
+signed main() {
+	ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+	
+	int t;
+	t = 1;
+	cin >> t;
+	while (t--) {
+		SOLVE::main();
+	}
+
+	return 0;
+}
