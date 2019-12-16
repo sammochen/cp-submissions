329ac08afd2fd3eb74c473b1262b2200bc280731
sammochen <samuelshchen@gmail.com>
2019-12-17 08:25:59 +1300

    added 10911

---
+++ uva/10911.cpp
@@ -0,0 +1,118 @@
+/*
+Original problem:
+Author: sammochen
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l;x<u;x++)
+#define RREP(x,l,u) for(ll x = l;x>=u;x--)
+
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+#define MP make_pair
+
+string to_string(string s) {return s;}
+string to_string(char c) {return "" + c;}
+template <typename A, typename B> string to_string(pair<A,B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+
+void in(string & s) {
+	char buf[100]; // note the 100 limit
+	ll a = scanf("%99s", buf);
+	s = buf;
+}
+
+void in(ll & x) {
+	ll a = scanf("%lld", &x);
+}
+
+template <typename A, typename B> void in(A & a, B & b) { in(a); in(b); }
+template <typename A, typename B, typename C> void in(A & a, B & b, C & c) { in(a); in(b); in(c); }
+
+const ll inf = (ll)1e18 + 5;
+namespace SOLVE {	
+	ll n;
+	vector<PLL> A;
+
+	db dist(ll a, ll b) {
+		db dx = A[a].first - A[b].first;
+		db dy = A[a].second - A[b].second;
+		return sqrt(dx * dx + dy * dy);
+	}
+
+	vector<db> dp;
+	double get(ll mask) {
+		if (dp[mask] != -1) return dp[mask];
+
+		ll start = -1;
+		REP(i,0,n) {
+			if (!((1LL << i) & mask)) {
+				start = i;
+				break;
+			}
+		}
+
+		if (start == -1) {
+			dp[mask] = 0;
+			return 0;
+		}
+
+		db best = inf;
+		REP(i,start + 1, n) {
+			if (!((1LL << i) & mask)) {
+				best = min(best, dist(start, i) + get(mask | (1LL << start) | (1LL << i)));
+			}
+		}
+		dp[mask] = best;
+		return best;
+	}
+
+	ll test = 1;
+	void main() {
+		
+		in(n);
+		if (n == 0) exit(0);
+
+		n *= 2;
+		string s;
+		A.clear();
+		A.resize(n);
+		dp.clear();
+		dp.resize((1LL << n), -1);
+		
+		REP(i,0,n) {
+			in(s, A[i].first, A[i].second);
+		}
+
+		db ans = get(0);
+		printf("Case %lld: %.2f\n", test++, ans);
+
+	}
+}
+
+
+signed main() {
+	while (1) {
+		SOLVE::main();
+	}
+
+	return 0;
+}
