b77c19d6813d6ef0c6b5a99f2ec38e7f53eeec8c
sammochen <samuelshchen@gmail.com>
2019-12-22 09:47:02 +1300

    added 11138

---
+++ uva/11138.cpp
@@ -0,0 +1,141 @@
+/*
+Original problem:
+Author: sammochen
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l;x<u;x++)
+#define RREP(x,l,u) for(ll x = l;x>=u;x--)
+
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+#define MP make_pair
+
+string to_string(string s) {return s;}
+string to_string(char c) {return "" + c;}
+template <typename A, typename B> string to_string(pair<A,B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+
+const ll inf = 1e9+7;
+
+namespace SOLVE {	
+	VLL bfs(VVLL & E, VVLL & W, ll s) {
+		VLL D(E.size(), inf);
+		queue<ll> Q;
+		D[s] = 0;
+		Q.push(s);
+		while (Q.size()) {
+			ll at = Q.front();
+			Q.pop();
+			for (ll to : E[at]) {
+				if (W[at][to] <= 0) continue;
+				if (D[to] != inf) continue;
+				D[to] = D[at] + 1;
+				Q.push(to); 
+			}
+		}
+		return D;
+	}
+
+	// returns the maxflow possible from at to t
+	ll dfs(VVLL & E, VVLL & W, VLL & D, ll at, ll t, ll f) {
+		if (at == t) {
+			return f;
+		}
+
+		for (ll to : E[at]) {
+			if (W[at][to] > 0 && D[to] == D[at] + 1) {
+				ll flow = dfs(E, W, D, to, t, min(f, W[at][to]));
+				if (flow > 0) {
+					W[at][to] -= flow;
+					W[to][at] += flow;
+					return flow;
+				}
+			}
+		}
+		return 0;
+	}
+
+	// returns the max flow
+	ll dinic(VVLL E, VVLL W, ll s, ll t) {
+		ll mf = 0;
+		while (1) {
+			// do bfs
+			VLL D = bfs(E, W, s);
+			if (D[t] == inf) return mf;
+
+			// do dfs 
+			while (1) {
+				ll f = dfs(E,W,D,s,t,inf);
+				mf += f;
+				if (f == 0) break;
+			}
+		}
+		return mf;
+	}
+
+	void addedge(VVLL & E, VVLL & W, ll a, ll b, ll w) {
+		E[a].push_back(b);
+		E[b].push_back(a);
+		W[a][b] = w;
+	}
+
+	ll test = 1;
+	void main() {
+		ll n, m;
+		cin >> n >> m;
+		ll s = n + m, t = n + m + 1;
+		VVLL E(n+m+2), W(n+m+2, VLL(n+m+2));
+
+		REP(i,0,n) {
+			REP(j,0,m) {
+				ll x;
+				cin >> x;
+				if (x == 1) {
+					addedge(E,W,i,n+j,1);
+				}
+			}
+		}
+
+		REP(i,0,n) {
+			addedge(E,W,s,i,1);
+		}
+
+		REP(j,0,m) {
+			addedge(E,W,n+j,t,1);
+		}
+
+		ll ans = dinic(E,W,s,t);
+		printf("Case %lld: a maximum of %lld nuts and bolts can be fitted together\n", test++, ans);
+
+	}
+}
+
+
+signed main() {
+	ll t;
+	cin >> t;
+	while (t--) {
+		SOLVE::main();
+	}
+
+	return 0;
+}
