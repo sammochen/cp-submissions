6a43ac533b5398c353930ff568e3c7ef760cb7bd
sammochen <samuelshchen@gmail.com>
2020-01-07 08:31:56 +1300

    added 11926

---
+++ uva/11926.cpp
@@ -0,0 +1,100 @@
+/*
+Original problem:
+Author: sammochen
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l;x<u;x++)
+#define RREP(x,l,u) for(ll x = l;x>=u;x--)
+
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+#define MP make_pair
+
+string to_string(string s) {return s;}
+string to_string(char c) {return "" + c;}
+template <typename A, typename B> string to_string(pair<A,B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+template <typename A> string to_string(set<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+
+void in(string & s) {
+	char buf[100]; // note the 100 limit
+	ll a = scanf("%99s", buf);
+	s = buf;
+}
+
+void in(ll & x) {
+	ll a = scanf("%lld", &x);
+}
+
+template <typename A, typename B> void in(A & a, B & b) { in(a); in(b); }
+template <typename A, typename B, typename C> void in(A & a, B & b, C & c) { in(a); in(b); in(c); }
+
+const ll inf = (ll)1e18 + 5;
+
+namespace SOLVE {	
+	void main() {
+		ll n, m;
+		while (cin >> n >> m, n||m) {
+			ll nax = 1000005;
+			VLL X(nax);
+			REP(i,0,n) {
+				ll a, b;
+				cin >> a >> b;
+				X[a]++;
+				X[b]--;
+			}
+			REP(j,0,m) {
+				ll a, b, c;
+				cin >> a >> b >> c;
+				while (1) {
+					if (a < nax) X[a]++;
+					if (b < nax) X[b]--;
+					a += c;
+					b += c;
+					if (a >= nax && b >= nax) break;
+				}
+			}
+
+			ll good = 1;
+			REP(i,0,1000001) {
+				if (X[i] >= 2) {
+					good = 0;
+					break;
+				}
+				X[i+1] += X[i];
+			}
+			if (good) cout << "NO CONFLICT" << endl;
+			else cout << "CONFLICT" << endl;
+		}
+	}
+}
+
+
+signed main() {
+	ll t;
+	t = 1;
+	REP(i,0,t) {
+		SOLVE::main();
+	}
+
+	return 0;
+}
