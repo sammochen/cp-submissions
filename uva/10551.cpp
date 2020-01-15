c5dcec8e448eb30f7eeb532341fd300d4d28dff7
sammochen <samuelshchen@gmail.com>
2020-01-16 08:38:32 +1300

    added 10551

---
+++ uva/10551.cpp
@@ -0,0 +1,115 @@
+/*
+Original problem:
+Author: sammochen
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l;x<u;x++)
+#define RREP(x,l,u) for(ll x = l;x>=u;x--)
+
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+#define MP make_pair
+
+string to_string(string s) {return s;}
+string to_string(char c) {return "" + c;}
+template <typename A, typename B> string to_string(pair<A,B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+template <typename A> string to_string(set<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+
+void in(string & s) {
+	char buf[100]; // note the 100 limit
+	ll a = scanf("%99s", buf);
+	s = buf;
+}
+
+void in(ll & x) {
+	ll a = scanf("%lld", &x);
+}
+
+template <typename A, typename B> void in(A & a, B & b) { in(a); in(b); }
+template <typename A, typename B, typename C> void in(A & a, B & b, C & c) { in(a); in(b); in(c); }
+
+const ll inf = (ll)1e18 + 5;
+
+namespace SOLVE {	
+	ll tov(char c) {
+		if (c >= '0' && c <= '9') return c - '0';
+		return c - 'A' + 10;
+	}
+
+	char toc(ll v) {
+		if (v < 10) return v + '0';
+		return v - 10 + 'A';
+	}
+
+	ll todec(string s, ll base) {
+		ll b = 1;
+		ll sum = 0;
+		RREP(i,s.length() - 1, 0) {
+			sum += b * tov(s[i]); 
+			b *= base;
+		}
+		return sum;
+	}
+
+	ll todecmod(string s, ll base, ll mod) {
+		ll b = 1;
+		ll sum = 0;
+		RREP(i,s.length() - 1, 0) {
+			sum += b * tov(s[i]) % mod; 
+			b *= base;
+			sum %= mod;
+			b %= mod;
+		}
+		return sum;
+	}
+
+	string tostring(ll dec, ll base) {
+		if (dec == 0) return "";
+		return tostring(dec / base, base) + toc(dec % base);
+	}
+
+	void main() {
+		ll b;
+		while (cin >> b, b) {
+			string p, m;
+			cin >> p >> m;
+			ll mod = todec(m, b);
+			ll rem = todecmod(p, b, mod);
+			string ans = tostring(rem, b);
+			if (ans == "") ans = "0";
+			cout << ans << endl;
+		}
+		
+	}
+}
+
+
+signed main() {
+	ll t;
+	t = 1;
+	REP(i,0,t) {
+		SOLVE::main();
+	}
+
+	return 0;
+}
