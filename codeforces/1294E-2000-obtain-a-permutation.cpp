66f03044604bd121991dda331386c35d88a256b1
sammochen <samuelshchen@gmail.com>
2020-01-24 08:51:21 +1300

    added 1294E-2000-obtain-a-permutation

---
+++ codeforces/1294E-2000-obtain-a-permutation.cpp
@@ -0,0 +1,103 @@
+/*
+Original problem:
+Author: sammochen
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l;x<u;x++)
+#define RREP(x,l,u) for(ll x = l;x>=u;x--)
+
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+#define MP make_pair
+
+string to_string(string s) {return s;}
+string to_string(char c) {return "" + c;}
+template <typename A, typename B> string to_string(pair<A,B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+template <typename A> string to_string(set<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+
+void in(string & s) {
+	char buf[100]; // note the 100 limit
+	ll a = scanf("%99s", buf);
+	s = buf;
+}
+
+void in(ll & x) {
+	ll a = scanf("%lld", &x);
+}
+
+template <typename A, typename B> void in(A & a, B & b) { in(a); in(b); }
+template <typename A, typename B, typename C> void in(A & a, B & b, C & c) { in(a); in(b); in(c); }
+
+const ll inf = (ll)1e18 + 5;
+
+namespace SOLVE {	
+	ll mod(ll x, ll m) {
+		while (x < 0) x += m;
+		return x % m;
+	}
+
+	void main() {
+		ll n, m;
+		cin >> m >> n;
+		// i will purposely read it the wrong way round
+		VVLL A(n, VLL(m));
+		REP(i,0,m) {
+			REP(j,0,n) {
+				cin >> A[j][i];
+				A[j][i]--;
+			}
+		}
+
+		ll ans = 0;
+
+		// for each row, add a freq to the ind to start
+		REP(i,0,n) {
+			// check for rotation
+			VLL cost(m, m);
+			
+			ll best = m;
+			REP(j,0,m) {
+				if (A[i][j] % n != i) continue;
+				if (A[i][j] >= m*n) continue;
+				
+				ll ind = mod(j - A[i][j] / n, m); // where it would start
+				
+				cost[ind]--;
+				best = min(best, cost[ind] + ind);
+			}
+			ans += best;
+		}
+		cout << ans << endl;
+	}
+}
+
+
+signed main() {
+	ll t;
+	t = 1;
+	REP(i,0,t) {
+		SOLVE::main();
+	}
+
+	return 0;
+}
