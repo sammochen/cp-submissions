3bd96b1a111f45e372f423feb872745557ebd0a0
sammochen <samuelshchen@gmail.com>
2020-02-17 17:51:13 +1300

    added 50-beautiful-quadruples

---
+++ hackerrank/50-beautiful-quadruples.cpp
@@ -0,0 +1,98 @@
+/*
+Original problem:
+Author: sammochen
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l;x<u;x++)
+#define RREP(x,l,u) for(ll x = l;x>=u;x--)
+
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+#define MP make_pair
+
+string to_string(string s) {return s;}
+string to_string(char c) {return "" + c;}
+template <typename A, typename B> string to_string(pair<A,B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+template <typename A> string to_string(set<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+
+void in(string & s) {
+	char buf[100]; // note the 100 limit
+	ll a = scanf("%99s", buf);
+	s = buf;
+}
+
+void in(ll & x) {
+	ll a = scanf("%lld", &x);
+}
+
+void in(VLL & A) {
+	REP(i,0,A.size()) in(A[i]);
+}
+
+template <typename A, typename B> void in(A & a, B & b) { in(a); in(b); }
+template <typename A, typename B, typename C> void in(A & a, B & b, C & c) { in(a); in(b); in(c); }
+
+const ll inf = (ll)1e18 + 5;
+
+VLL A(4);
+ll dp[4096][3001][4];
+
+namespace SOLVE {	
+	ll get(ll x, ll lower, ll ind) {
+		if (ind == 4) {
+			return x != 0;
+		}
+		if (lower > A[ind]) return 0;
+		if (dp[x][lower][ind] != -1) return dp[x][lower][ind];
+
+		ll ans = 0;
+		ans += get(x ^ lower, lower, ind + 1) + get(x, lower + 1, ind);
+
+		dp[x][lower][ind] = ans;
+		return ans;
+	}
+
+	void main() {
+		memset(dp, -1, sizeof(dp));
+
+		in(A);
+		sort(A.begin(), A.end());
+
+		ll ans = get(0,1,0);
+		cout << ans << endl;
+
+
+
+	}
+}
+
+
+signed main() {
+	ll t;
+	t = 1;
+	REP(i,0,t) {
+		SOLVE::main();
+	}
+
+	return 0;
+}
