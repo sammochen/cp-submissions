5e56d9a32210f89d7e37669aa4cf27bf5ef882a7
sammochen <samuelshchen@gmail.com>
2020-02-20 23:19:06 +1300

    added 00696

---
+++ uva/00696.cpp
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l;x<u;x++)
+#define RREP(x,l,u) for(ll x = l;x>=u;x--)
+
+#define PLL pair<ll,ll>
+#define MP make_pair
+
+string to_string(string s) {return s;}
+string to_string(char c) {string s = string(1, c);return s;}
+template <typename A, typename B> string to_string(pair<A,B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+template <typename A> string to_string(set<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+
+const ll inf = (ll)1e18 + 5;
+
+namespace SOLVE {	
+	ll solve(ll n, ll m) {
+		if (n > m) swap(n, m);
+		ll ans;
+		if (n == 1) ans = m;
+		else if (n == 2) ans = m / 4 * 4 + (m % 4 == 1 ? 2 : m % 4 >= 2 ? 4 : 0);
+		else ans = ((n*m)+1)/2;
+		return ans;
+	}
+	void main() {
+		ll n, m;
+		while (cin >> n >> m, n||m) {
+			printf("%lld knights may be placed on a %lld row %lld column board.\n", solve(n,m), n, m);
+		}
+	}
+}
+
+
+signed main() {
+	ll t;
+	t = 1;
+	REP(i,0,t) {
+		SOLVE::main();
+	}
+
+	return 0;
+}
