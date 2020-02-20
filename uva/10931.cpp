1bce42b096fa8207a554ab94d942427dcf636009
sammochen <samuelshchen@gmail.com>
2020-02-21 02:14:58 +1300

    added 10931

---
+++ uva/10931.cpp
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l;x<u;x++)
+#define RREP(x,l,u) for(ll x = l;x>=u;x--)
+
+#define PLL pair<ll,ll>
+#define MP make_pair
+
+string to_string(string s) {return s;}
+string to_string(char c) {string s = string(1, c);return s;}
+template <typename A, typename B> string to_string(pair<A,B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+template <typename A> string to_string(set<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+
+const ll inf = (ll)1e18 + 5;
+
+namespace SOLVE {	
+	string binary(ll x) {
+		if (x == 0) return "";
+		if (x % 2) return binary(x/2) + "1";
+		else return binary(x/2) + "0";
+	}
+
+	ll parity(ll x) {
+		string b = binary(x);
+		ll ans = 0;
+		for (char c : b) if (c == '1') ans++;
+		return ans;
+	}
+	void main() {
+		ll n;
+		while (cin >> n, n) {
+			printf("The parity of %s is %lld (mod 2).\n", binary(n).c_str(), parity(n));
+		}
+	}
+}
+
+
+signed main() {
+	ll t;
+	t = 1;
+	REP(i,0,t) {
+		SOLVE::main();
+	}
+
+	return 0;
+}
