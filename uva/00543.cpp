e27981027a14beb15b33d1132f741bf4d382a4a8
sammochen <samuelshchen@gmail.com>
2020-03-11 21:55:26 +1300

    added 00543

---
+++ uva/00543.cpp
@@ -0,0 +1,73 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l; x < u; x++)
+#define RREP(x,l,u) for(ll x = l; x >= u; x--)
+
+string to_string(string s) {return s;}
+string to_string(char c) {string s = string(1, c);return s;}
+template <typename A, typename B> string to_string(pair<A,B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+template <typename A> string to_string(set<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+typedef pair<ll,ll> PLL;
+
+const ll inf = (ll)1e18 + 5;
+
+ll nax = 1005;
+VLL p(nax*nax, 1);
+VLL P;
+void init() {
+	p[0] = 0;
+	p[1] = 0;
+
+	for (ll x = 2; x < nax * nax; x++) {
+		if (p[x] == 0) {
+			continue;
+		}
+        P.push_back(x);
+		
+		for (ll f = 2; x * f < nax * nax; f++) {
+			p[x*f] = 0;
+		}
+	}
+}
+
+void solve() {
+    ll n;
+    while (cin >> n, n) {
+        ll good = 0;
+        REP(i,0,P.size()) {
+            if (P[i] > n) break;
+            
+            if (p[n-P[i]]) {
+                printf("%lld = %lld + %lld\n", n, P[i], n-P[i]);
+                good = 1;
+                break;
+            }
+        }
+        if (!good) printf("Goldbach's conjecture is wrong.\n");
+    }
+}
+
+signed main() {
+	init();
+	ll t = 1;
+	REP(i,0,t) solve();
+	return 0;
+}
