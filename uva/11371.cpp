33a8b9848046eb6671a53f975f99fa1de80581dd
sammochen <samuelshchen@gmail.com>
2020-03-13 08:57:39 +1300

    added 11371

---
+++ uva/11371.cpp
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l; x < u; x++)
+#define RREP(x,l,u) for(ll x = l; x >= u; x--)
+
+string to_string(string s) {return s;}
+string to_string(char c) {string s = string(1, c);return s;}
+template <typename A, typename B> string to_string(pair<A,B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+template <typename A> string to_string(set<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+typedef pair<ll,ll> PLL;
+
+const ll inf = (ll)1e18 + 5;
+
+void solve() {
+    string s;
+    while (cin >> s) {
+        string a = s, b = s;
+        sort(a.rbegin(), a.rend());
+        sort(b.begin(), b.end());
+
+        // if b has leading 0, need to find the first non-zero and move that to the front
+        if (b[0] == '0') {
+            REP(i,0,b.length()) {
+                if (b[i] != '0') {
+                    b = b[i] + b.substr(0,i) + b.substr(i+1);
+                    break;
+                }
+            }
+        }
+
+        ll v = stoll(a) - stoll(b);
+        printf("%s - %s = %lld = 9 * %lld\n", a.c_str(), b.c_str(), v, v/9);
+    }
+}
+
+signed main() {
+	ll t = 1;
+	REP(i,0,t) solve();
+	return 0;
+}
