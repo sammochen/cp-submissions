5deec12f0d35aadf6f82251109bb3dfaa822eca2
sammochen <samuelshchen@gmail.com>
2020-03-13 17:45:39 +1300

    added 11401

---
+++ uva/11401.cpp
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define REP(x,l,u) for(ll x = l; x < u; x++)
+#define RREP(x,l,u) for(ll x = l; x >= u; x--)
+
+string to_string(string s) {return s;}
+string to_string(char c) {string s = string(1, c);return s;}
+template <typename A, typename B> string to_string(pair<A,B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+template <typename A> string to_string(set<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+
+void debug_out() {cerr << endl;}
+template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
+
+#ifndef ONLINE_JUDGE
+#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do { } while (false)
+#endif
+
+typedef  long long ll;
+typedef double db;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+typedef pair<ll,ll> PLL;
+
+const ll inf = (ll)1e18 + 5;
+
+void solve() {
+    ll n = 1000005;
+    VLL dp(n);
+    dp[3] = 0;
+    dp[4] = 1;
+    REP(i,5,n) {
+        // how many triangles can you make with the longest side being i
+        // how many pairs add to more than 5
+        // triangle numbers
+        ll b = i/2 + 1; 
+        dp[i] = dp[i-1] + i*(i-1) -(i+1)*(i-b) - b*(b-1);
+    }
+
+    ll x;
+    while (cin >> x, x >= 3) {
+        printf("%lld\n", dp[x]);
+    }
+}
+
+signed main() {
+	ll t = 1;
+	REP(i,0,t) solve();
+	return 0;
+}
