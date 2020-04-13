edbb2b94e4dddc0c050fa8c62fc9856014835ce6
sammochen <samuelshchen@gmail.com>
2020-04-13 14:38:00 +1200

    added 1234C

--- codeforces/1234C.cpp
+++ codeforces/1234C.cpp
@@ -1,24 +1,16 @@
-/*
-Original problem:
-Author: sammochen
-*/
-
 #define _CRT_SECURE_NO_WARNINGS
 #include "bits/stdc++.h"
 
 using namespace std;
 
-#define REP(x,l,u) for(ll x = l;x<u;x++)
-#define RREP(x,l,u) for(ll x = l;x>=u;x--)
-
-#define PII pair<int,int>
-#define PLL pair<ll,ll>
-#define MP make_pair
+#define REP(x,l,u) for(ll x = l; x < u; x++)
+#define RREP(x,l,u) for(ll x = l; x >= u; x--)
 
 string to_string(string s) {return s;}
-string to_string(char c) {return "" + c;}
-template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+string to_string(char c) {string s = string(1, c);return s;}
 template <typename A, typename B> string to_string(pair<A,B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
+template <typename A> string to_string(set<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
 
 void debug_out() {cerr << endl;}
 template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
@@ -29,49 +21,43 @@ template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { c
 #define debug(...) do { } while (false)
 #endif
 
-typedef  long long ll;
+typedef long long ll;
 typedef double db;
 typedef vector<ll> VLL;
 typedef vector<VLL> VVLL;
+typedef pair<ll,ll> PLL;
 
-namespace SOLVE {	
-	void main() {
-		ll n; 
-		string a, b;
-		cin >> n >> a >> b;
-		VLL dp(2, 0); // dp[i] is 1 if reachable
-		dp[0] = 1;
+const ll inf = (ll)1e18 + 5;
 
-		REP(i,0,n) {
-			VLL next(2);
 
-			if (a[i] <= '2' && b[i] <= '2') {
-				next = dp;
-			} else if (a[i] <= '2') {
-				next[0] = dp[0];
-			} else if (b[i] <= '2') {
-				next[1] = dp[1];
-			} else {
-				next[0] = dp[1];
-				next[1] = dp[0];
+ll bent(char c) {
+	return c >= '3' && c <= '6';
+}
+void solve() {
+	ll n;
+	cin >> n;
+	vector<string> A(2);
+	cin >> A[0] >> A[1];
+
+	VVLL dp(n+1, VLL(2));
+	dp[0][0] = 1;
+	dp[0][1] = 0;
+
+	REP(i,0,n) {
+		REP(j,0,2) {
+			if (!bent(A[j][i])) dp[i+1][j] |= dp[i][j];
+			else {
+				if (bent(A[!j][i])) dp[i+1][j] |= dp[i][!j];
 			}
-
-			dp = next;
 		}
-
-		string ans = dp[1] ? "YES" : "NO";
-		cout << ans << endl;
 	}
+	if (dp[n][1]) cout << "YES" << endl;
+	else cout << "NO" << endl;
 }
 
-
 signed main() {
-	int t;
-	// t = 1;
+	ll t = 1;
 	cin >> t;
-	while (t--) {
-		SOLVE::main();
-	}
-
+	REP(i,0,t) solve();
 	return 0;
 }
