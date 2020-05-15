824a9ebc555224fdd15a46dfa70ae6bbb65e5644
sammochen <samuelshchen@gmail.com>
2020-05-16 09:52:34 +1200

    added 1348D

---
+++ codeforces/1348D.cpp
@@ -0,0 +1,93 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+
+typedef long long ll;
+typedef double db;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<ll> VLL;
+typedef vector<VLL> VVLL;
+typedef pair<ll,ll> PLL;
+typedef map<ll,ll> MLL;
+typedef set<ll> SLL;
+
+#define REP(x,l,u) for(ll x = l; x < u; x++)
+#define RREP(x,l,u) for(ll x = l; x >= u; x--)
+#define FE(x,a) for (auto x : a)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define mst(x,v) memset(x, v, sizeof(x))
+#define sz(x) (ll)x.size()
+#define fi first
+#define se second
+#define pb push_back
+
+#define umap unordered_map
+#define uset unordered_set
+#define mset multiset
+#define pqueue priority_queue
+
+struct DEBUG {
+	string open = "[", close = "]", sep = ", ";
+
+	string f(string s) { return s; }
+	string f(char c) { return string(1, c); }
+	string f(int x) { return to_string(x); }
+	string f(ll x) { return to_string(x); }
+	template <typename A, typename B> string f(pair<A,B> p) { return open + f(p.first) + sep + f(p.second) + close; }
+	template <typename A> string f(A v) { string s = open; for (auto a : v) { if (s != open) { s += sep; } s += f(a); } s += close; return s; } 
+
+	void show() {cout << endl;}
+	template <typename Head, typename... Tail> void show(Head H, Tail... T) { cout << " " << f(H); show(T...); }
+};
+
+#define debug(...) do { DEBUG deb; cout << "[" << #__VA_ARGS__ << "]:", deb.show(__VA_ARGS__); } while (false)
+
+const ll inf = (ll)1e18 + 5;
+const ll mod = 1e9+7;
+
+VLL di = {0,0,1,-1,1,-1,1,-1};
+VLL dj = {1,-1,0,0,-1,-1,1,1};
+ll ob(ll i, ll n) { return i < 0 || i >= n; } // out of bounds
+ll tp(ll x) { return (1LL<<x); }
+ll roundup(ll a, ll b) { return a % b ? a/b + 1 : a/b; }
+
+void upmin(ll & x, ll v) { x = min(x, v); }
+void upmax(ll & x, ll v) { x = max(x, v); }
+
+void solve() {
+	ll n;
+	cin >> n;
+
+	ll sum = 0;
+	VLL ans;
+	REP(i,0,inf) {
+		sum += tp(i);
+		if (sum > n) {
+			sum -= tp(i);
+			break;
+		}
+		ans.pb(tp(i));
+	}
+
+	if (sum != n) {
+		ans.pb(n - sum);
+	}
+	sort(all(ans));
+
+	cout << sz(ans) - 1 << endl;
+	REP(i,1,sz(ans)) {
+		cout << ans[i] - ans[i-1] << ' ';
+	}
+	cout << endl;
+
+}
+
+signed main() {
+	ll t = 1;
+	cin >> t;
+	REP(i,0,t) solve();
+	return 0;
+}
