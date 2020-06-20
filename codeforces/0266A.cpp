fc522e4a695bc4d2e4731c4a9e06a2e0bff7e893
sammochen <samuelshchen@gmail.com>
2020-06-20 15:23:11 +1200

    added 0266A

--- codeforces/0266A.cpp
+++ codeforces/0266A.cpp
@@ -1,63 +1,112 @@
-/*
-Original problem:
-Author: sammochen
-*/
-
 #define _CRT_SECURE_NO_WARNINGS
 #include "bits/stdc++.h"
 
 using namespace std;
-
-#define REP(x,l,u) for(ll x = l;x<u;x++)
-#define RREP(x,l,u) for(ll x = l;x>=u;x--)
-
-#define PII pair<int,int>
-#define PLL pair<ll,ll>
-#define MP make_pair
-
-string to_string(string s) {return s;}
-string to_string(char c) {return "" + c;}
-template <typename A> string to_string(vector<A> v) { string s = "("; int first = 1; for (A a : v) { if (!first) { s += ", "; } first = 0; s += to_string(a); } s += ")"; return s; }
-
-void debug_out() {cerr << endl;}
-template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
-
-#ifndef ONLINE_JUDGE
-#define debug(...) do { cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__); } while (false)
-#else
-#define debug(...) do { } while (false)
-#endif
-
-typedef  long long ll;
+typedef long long ll;
 typedef double db;
-typedef vector<ll> VLL;
+typedef vector<string> VS;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<ll> VLL; 
 typedef vector<VLL> VVLL;
+typedef pair<ll,ll> PLL;
+typedef map<ll,ll> MLL;
+typedef set<ll> SLL;
+typedef queue<ll> QLL;
 
-const ll inf = 1e9+7;
+#define rep(x,l,u) for(ll x = l; x < u; x++)
+#define rrep(x,l,u) for(ll x = l; x >= u; x--)
+#define fe(x,a) for (auto x : a)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define mst(x,v) memset(x, v, sizeof(x))
+#define sz(x) (ll)x.size()
+#define lg(x) (ll)x.length()
+#define fi first
+#define se second
+#define pb push_back
 
-namespace SOLVE {
-	void main() {
-		ll n;
-		cin >> n;
-		string s;
-		cin >> s;
+#define umap unordered_map
+#define uset unordered_set
+#define mset multiset
+#define pqueue priority_queue
 
-		ll ans = 0;
-		REP(i,1,n) {
-			ans += (s[i] == s[i-1]);
-		}
-		cout << ans << endl;
+struct FASTINPUT {
+	void f(string & s) { char buf[200005]; ll a = scanf("%s", buf); a++; s = buf; }
+	void f(ll & x) { ll a = scanf("%lld", &x); a++; }
+	void f(int & x) { ll a = scanf("%d", &x); a++; }
+	template <typename A, typename B> void f(pair<A,B> & p) { f(p.first); f(p.second); }
+	template <typename A> void f(vector<A> & x) { for (auto & y : x) f(y); }
+	
+	void read() {}
+	template <typename Head, typename... Tail> void read(Head & H, Tail & ... T) { f(H); read(T...); }
+};
+
+#define fin(...) do { FASTINPUT inp; inp.read(__VA_ARGS__); } while (false)
+
+struct FASTOUTPUT {
+	void f(string s) { printf("%s", s.c_str()); }
+	void f(ll x) { printf("%lld", x); }
+    void f(int x) { printf("%d", x); }
+    void f(char x) { printf("%c", x); }
+    void f(const char* a) { printf("%s", a); }
+	
+	void print() {}
+	template <typename Head, typename... Tail> void print(Head H, Tail ... T) { f(H); print(T...); }
+};
+
+#define fout(...) do { FASTOUTPUT out; out.print(__VA_ARGS__); } while (false)
+
+struct DEBUG {
+	string open = "[", close = "]", sep = ", ";
+
+	string f(string s) { return s; }
+	string f(char c) { return string(1, c); }
+	string f(int x) { return to_string(x); }
+	string f(ll x) { return to_string(x); }
+	string f(db x) { return to_string(x); }
+	string f(const char* a) { return a; }
+	template <typename A, typename B> string f(pair<A,B> p) { return open + f(p.first) + sep + f(p.second) + close; }
+	template <typename A> string f(A v) { string s = open; for (auto a : v) { if (s != open) { s += sep; } s += f(a); } s += close; return s; } 
+
+	void show() {cout << endl;}
+	template <typename Head, typename... Tail> void show(Head H, Tail... T) { cout << " " << f(H); show(T...); }
+};
+
+#define debug(...) do { DEBUG deb; cout << "[" << #__VA_ARGS__ << "]:", deb.show(__VA_ARGS__); } while (false)
+
+VLL di = {0,0,1,-1,1,-1,1,-1};
+VLL dj = {1,-1,0,0,-1,-1,1,1};
+ll ob(ll i, ll n) { return i < 0 || i >= n; } // out of bounds
+ll tp(ll x) { return (1LL<<x); }
+ll roundup(ll a, ll b) { return a % b ? a/b + 1 : a/b; }
+template <typename A, typename B> ll exist(A a, B b) { return a.find(b) != a.end(); }
+vector<string> ssplit(string s) { vector<string> ans; stringstream ss; ss << s; while (ss >> s) ans.pb(s); return ans; }
+
+void upmin(ll & x, ll v) { x = min(x, v); }
+void upmax(ll & x, ll v) { x = max(x, v); }
+
+const ll inf = (ll)1e18 + 5;
+const ll mod = 1e9+7;
+const ll mod2 = 998244353;
+const db eps = 1e-10;
+const string nl = "\n";
+
+
+void solve() {
+	ll n;
+	string s;
+	fin(n, s);
+
+	ll ans = 0;
+	rep(i,1,n) {
+		if (s[i] == s[i-1]) ans++;
 	}
+	fout(ans, nl);
 }
 
-
 signed main() {
-	int t;
-	t = 1;
-	// cin >> t;
-	while (t--) {
-		SOLVE::main();
-	}
-
+	ll t = 1;
+	rep(i,0,t) solve();
 	return 0;
 }
