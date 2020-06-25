779a69b793f9f4aadca03329ffb1e5b5d84f3da7
sammochen <samuelshchen@gmail.com>
2020-06-25 16:18:45 +1200

    added 1368E

---
+++ codeforces/1368E.cpp
@@ -0,0 +1,153 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+typedef long long ll;
+typedef double db;
+typedef vector<string> VS;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<ll> VLL; 
+typedef vector<VLL> VVLL;
+typedef pair<ll,ll> PLL;
+typedef map<ll,ll> MLL;
+typedef set<ll> SLL;
+typedef queue<ll> QLL;
+
+#define rep(x,l,u) for(ll x = l; x < u; x++)
+#define rrep(x,l,u) for(ll x = l; x >= u; x--)
+#define fe(x,a) for (auto x : a)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define mst(x,v) memset(x, v, sizeof(x))
+#define sz(x) (ll)x.size()
+#define lg(x) (ll)x.length()
+#define fi first
+#define se second
+#define pb push_back
+
+#define umap unordered_map
+#define uset unordered_set
+#define mset multiset
+#define pqueue priority_queue
+
+struct FASTINPUT {
+	void f(string & s) { char buf[200005]; ll a = scanf("%s", buf); a++; s = buf; }
+	void f(ll & x) { ll a = scanf("%lld", &x); a++; }
+	void f(int & x) { ll a = scanf("%d", &x); a++; }
+	template <typename A, typename B> void f(pair<A,B> & p) { f(p.first); f(p.second); }
+	template <typename A> void f(vector<A> & x) { for (auto & y : x) f(y); }
+	
+	void read() {}
+	template <typename Head, typename... Tail> void read(Head & H, Tail & ... T) { f(H); read(T...); }
+};
+
+struct FASTOUTPUT {
+	void f(string s) { printf("%s", s.c_str()); }
+	void f(ll x) { printf("%lld", x); }
+    void f(int x) { printf("%d", x); }
+    void f(char x) { printf("%c", x); }
+    void f(const char* a) { printf("%s", a); }
+	
+	void print() {}
+	template <typename Head, typename... Tail> void print(Head H, Tail ... T) { f(H); print(T...); }
+};
+
+struct DEBUG {
+	string open = "[", close = "]", sep = ", ";
+
+	string f(string s) { return s; }
+	string f(char c) { return string(1, c); }
+	string f(int x) { return to_string(x); }
+	string f(ll x) { return to_string(x); }
+	string f(db x) { return to_string(x); }
+	string f(const char* a) { return a; }
+	template <typename A, typename B> string f(pair<A,B> p) { return open + f(p.first) + sep + f(p.second) + close; }
+	template <typename A> string f(A v) { string s = open; for (auto a : v) { if (s != open) { s += sep; } s += f(a); } s += close; return s; } 
+
+	void show() {cout << endl;}
+	template <typename Head, typename... Tail> void show(Head H, Tail... T) { cout << " " << f(H); show(T...); }
+};
+
+#define fin(...) do { FASTINPUT inp; inp.read(__VA_ARGS__); } while (false)
+#define fout(...) do { FASTOUTPUT out; out.print(__VA_ARGS__); } while (false)
+#define debug(...) do { DEBUG deb; cout << "[" << #__VA_ARGS__ << "]:", deb.show(__VA_ARGS__); } while (false)
+
+VLL di = {0,0,1,-1,1,-1,1,-1};
+VLL dj = {1,-1,0,0,-1,-1,1,1};
+ll ob(ll i, ll n) { return i < 0 || i >= n; } // out of bounds
+ll tp(ll x) { return (1LL<<x); }
+ll roundup(ll a, ll b) { return a % b ? a/b + 1 : a/b; }
+template <typename A, typename B> ll exist(A a, B b) { return a.find(b) != a.end(); }
+vector<string> ssplit(string s) { vector<string> ans; stringstream ss; ss << s; while (ss >> s) ans.pb(s); return ans; }
+
+void upmin(ll & x, ll v) { x = min(x, v); }
+void upmax(ll & x, ll v) { x = max(x, v); }
+
+const ll inf = (ll)1e18 + 5;
+const ll mod = 1e9+7;
+const ll mod2 = 998244353;
+const db eps = 1e-10;
+const string nl = "\n";
+
+
+void solve() {
+	ll n, m;
+	fin(n, m);
+	
+	VVLL E(n);
+	VLL indeg(n);
+	rep(i,0,m) {
+		ll a, b;
+		fin(a, b);
+		a--; b--;
+
+		E[a].pb(b);
+		indeg[b]++;
+	}
+
+	QLL Q;
+	rep(i,0,n) {
+		if (indeg[i] == 0) {
+			Q.push(i);
+		}
+	}
+
+	VLL order;
+
+	while (sz(Q)) {
+		ll at = Q.front(); Q.pop();
+		order.pb(at);
+
+		fe(to, E[at]) {
+			indeg[to]--;
+			if (indeg[to] == 0) Q.push(to);
+		}
+	}
+
+	VVLL par(n, VLL(3));
+	VLL ans;
+
+	fe(x, order) {
+		ll cur = -1;
+		if (par[x][0] == 0 && par[x][1] == 0) cur = 0;
+		else if (par[x][0] && par[x][1] == 0) cur = 1;
+		else cur = 2;
+
+		if (cur == 2) ans.pb(x);
+		fe(to, E[x]) {
+			par[to][cur] = 1;
+		}
+	}
+	
+	fout(sz(ans), nl);
+	fe(x, ans) fout(x+1, ' ');
+	fout(nl);
+}
+
+signed main() {
+	ll t = 1;
+	fin(t);
+	rep(i,0,t) solve();
+	return 0;
+}
