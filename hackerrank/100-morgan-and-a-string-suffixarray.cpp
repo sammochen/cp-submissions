545c38cf73da1fc963fff254e1444df5c89fb8ee
sammochen <samuelshchen@gmail.com>
2020-07-08 06:59:43 +1200

    added 100-morgan-and-a-string-suffixarray

---
+++ hackerrank/100-morgan-and-a-string-suffixarray.cpp
@@ -0,0 +1,215 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "bits/stdc++.h"
+
+using namespace std;
+typedef long long ll;
+typedef double db;
+typedef vector<string> VS;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<ll> VLL; 
+typedef vector<VLL> VVLL;
+typedef vector<VVLL> VVVLL;
+typedef pair<ll,ll> PLL;
+typedef map<ll,ll> MLL;
+typedef set<ll> SLL;
+typedef queue<ll> QLL;
+
+#define rep(x,l,u) for(ll x = l; x < u; x++)
+#define rrep(x,l,u) for(ll x = l; x >= u; x--)
+#define fe(x,a) for (auto x : a)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define mst(x,v) memset(x, v, sizeof(x))
+#define sz(x) (ll)x.size()
+#define lg(x) (ll)x.length()
+#define fi first
+#define se second
+#define pb push_back
+
+#define umap unordered_map
+#define uset unordered_set
+#define mset multiset
+#define pqueue priority_queue
+
+struct FASTINPUT {
+	void f(string & s) { char buf[400005]; ll a = scanf("%s", buf); a++; s = buf; }
+	void f(ll & x) { ll a = scanf("%lld", &x); a++; }
+	void f(int & x) { ll a = scanf("%d", &x); a++; }
+	void f(double & x) { ll a = scanf("%lf", &x); a++; }
+	template <typename A, typename B> void f(pair<A,B> & p) { f(p.first); f(p.second); }
+	template <typename A> void f(vector<A> & x) { for (auto & y : x) f(y); }
+	
+	void read() {}
+	template <typename Head, typename... Tail> void read(Head & H, Tail & ... T) { f(H); read(T...); }
+};
+
+struct FASTOUTPUT {
+	void f(string s) { printf("%s", s.c_str()); }
+	void f(ll x) { printf("%lld", x); }
+    void f(int x) { printf("%d", x); }
+	void f(double x) { printf("%.20f", x); }
+    void f(char x) { printf("%c", x); }
+    void f(const char* a) { printf("%s", a); }
+	
+	void print() {}
+	template <typename Head, typename... Tail> void print(Head H, Tail ... T) { f(H); print(T...); }
+};
+
+struct DEBUG {
+	string open = "[", close = "]", sep = ", ";
+
+	string f(string s) { return s; }
+	string f(char c) { return string(1, c); }
+	string f(int x) { return to_string(x); }
+	string f(ll x) { return to_string(x); }
+	string f(db x) { return to_string(x); }
+	string f(const char* a) { return a; }
+	template <typename A, typename B> string f(pair<A,B> p) { return open + f(p.first) + sep + f(p.second) + close; }
+	template <typename A> string f(A v) { string s = open; for (auto a : v) { if (s != open) { s += sep; } s += f(a); } s += close; return s; } 
+
+	void show() {cout << endl;}
+	template <typename Head, typename... Tail> void show(Head H, Tail... T) { cout << " " << f(H); show(T...); }
+};
+
+#define fin(...) do { FASTINPUT inp; inp.read(__VA_ARGS__); } while (false)
+#define fout(...) do { FASTOUTPUT out; out.print(__VA_ARGS__); } while (false)
+#define debug(...) do { DEBUG deb; cout << "[" << #__VA_ARGS__ << "]:", deb.show(__VA_ARGS__); } while (false)
+
+VLL di = {0,0,1,-1,1,-1,1,-1};
+VLL dj = {1,-1,0,0,-1,-1,1,1};
+ll ob(ll i, ll n) { return i < 0 || i >= n; } // out of bounds
+ll tp(ll x) { return (1LL<<x); }
+ll roundup(ll a, ll b) { return a % b ? a/b + 1 : a/b; }
+template <typename A, typename B> ll exist(A a, B b) { return a.find(b) != a.end(); }
+vector<string> ssplit(string s) { vector<string> ans; stringstream ss; ss << s; while (ss >> s) ans.pb(s); return ans; }
+ll makemod(ll & x, ll m) { x %= m; x += m; x %= m; return x; }
+ll powmod(ll a, ll b, ll m) {
+	if (b == 0) return 1;
+	ll h = powmod(a, b/2, m);
+	ll ans = h*h%m;
+	return b%2 ? ans*a%m : ans;
+}
+
+void upmin(ll & x, ll v) { x = min(x, v); }
+void upmax(ll & x, ll v) { x = max(x, v); }
+
+const ll inf = (ll)1e18 + 5;
+const ll mod = 1e9+7;
+const ll mod2 = 998244353;
+const db eps = 1e-10;
+const db pi = acos(0) * 2;
+const string nl = "\n";
+
+
+
+const ll nax = 4e5 + 5;
+ll P[nax];
+ll L[nax][2][3];
+ll sa[nax];
+ll lcp[nax];
+
+template<typename A> void arrdebug(A a[], ll n) {
+	rep(i,0,n) {
+		fout(a[i], ' ');
+	}
+	fout(nl);
+}
+
+void radixsort(ll ind, ll n, ll bucket, ll from, ll to) {
+	VLL cnt(bucket); 
+	rep(i,0,n) {
+		cnt[L[i][from][ind]+1]++;
+	}
+
+	rep(i,1,bucket) cnt[i] += cnt[i-1];
+	
+	rrep(i,n-1,0) {
+		--cnt[L[i][from][ind]+1];
+
+		rep(j,0,3) {
+			L[cnt[L[i][from][ind]+1]][to][j] = L[i][from][j];
+		}
+	}
+}
+
+void suffixarray(string & s) {
+	ll n = lg(s);
+	ll k = log2(n) + 2;
+	
+	rep(i,0,n) {
+		P[i] = s[i]; // get initial rank
+	}
+
+	rep(i,1,k) {
+		rep(j,0,n) {
+			L[j][0][0] = P[j]; 
+			L[j][0][1] = j + tp(i-1) < n ? P[j+tp(i-1)] : -1;
+			L[j][0][2] = j;
+		}
+
+		ll buckets = n+1;
+		if (i == 1) upmax(buckets, 256);
+		radixsort(1, n, buckets, 0, 1);
+		radixsort(0, n, buckets, 1, 0);	
+		
+		rep(j,0,n) {
+			P[L[j][0][2]] = j && L[j-1][0][0] == L[j][0][0] && L[j-1][0][1] == L[j][0][1] ? P[L[j-1][0][2]] : j;
+		}
+	}
+
+	rep(i,0,n) {
+		sa[P[i]] = i;
+	}
+
+	VLL ind(n);
+	rep(i,0,n) {
+		ind[sa[i]] = i;
+	}
+
+	ll res = 0;
+	rep(i,0,n-1) {
+		ll index = ind[i] - 1;
+		if (index < 0) continue;
+		ll j = sa[index];
+
+		rep(k,res,nax) {
+			if (s[i+k] == s[j+k]) res++;
+			else break;
+		}
+		lcp[index] = res;
+
+		if (res) res--;
+	}
+}
+
+void solve() {
+	string s, t;
+	fin(s, t);
+
+	ll n = lg(s), m = lg(t);
+
+	string A = s + "a" + t + "a";
+	suffixarray(A);
+
+	string ans;
+	ll i = 0, j = 0;
+	while (i < n+1 && j < m+1) {
+		char c = '?';
+		if (P[i] < P[n+1+j]) {
+			c = s[i++];
+		} else {
+			c = t[j++];
+		}
+		ans.pb(c);
+	}
+
+	fout(ans, nl);
+}
+
+signed main() {
+	ll t = 1;
+	fin(t);
+	rep(i,0,t) solve();
+	return 0;
+}
