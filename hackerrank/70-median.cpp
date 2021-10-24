3d005c4c4e214160183f3e0450c87da6a919d8ed
Shih-Hao Chen <samuelshchen@gmail.com>
2021-10-25 10:06:35 +1300

    added 70-median

---
+++ hackerrank/70-median.cpp
@@ -0,0 +1,204 @@
+// clang-format off
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef double db;
+typedef vector<string> VS;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<ll> VLL; 
+typedef vector<VLL> VVLL;
+typedef vector<VVLL> VVVLL;
+typedef pair<ll,ll> PLL;
+typedef map<ll,ll> MLL;
+typedef set<ll> SLL;
+typedef queue<ll> QLL;
+
+#define rep(x,l,u) for(ll x = l; x < u; x++)
+#define rrep(x,l,u) for(ll x = l; x >= u; x--)
+#define fe(x,a) for (auto x : a)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define mst(x,v) memset(x, v, sizeof(x))
+#define sz(x) (ll)x.size()
+#define lg(x) (ll)x.length()
+#define fi first
+#define se second
+#define pb push_back
+
+#define umap unordered_map
+#define uset unordered_set
+#define mset multiset
+#define pqueue priority_queue
+
+struct DEBUG {
+	string open = "[", close = "]", sep = ", ";
+
+	string f(string s) { return s; }
+	string f(char c) { return string(1, c); }
+	string f(int x) { return to_string(x); }
+	string f(ll x) { return to_string(x); }
+	string f(db x) { return to_string(x); }
+	string f(const char* a) { return a; }
+	template <typename A, typename B> string f(pair<A,B> p) { return open + f(p.first) + sep + f(p.second) + close; }
+	template <typename A> string f(A v) { string s = open; for (auto a : v) { if (s != open) { s += sep; } s += f(a); } s += close; return s; } 
+
+	void show() {cerr << endl;}
+	template <typename Head, typename... Tail> void show(Head H, Tail... T) { cerr << " " << f(H); show(T...); }
+};
+
+#ifdef SAM
+#define debug(...) do { DEBUG deb; cerr << "[" << #__VA_ARGS__ << "]:", deb.show(__VA_ARGS__); } while (false)
+#else
+#define debug(...) do {} while (false)
+#endif
+
+VLL di = {0,0,1,-1,1,-1,1,-1};
+VLL dj = {1,-1,0,0,-1,-1,1,1};
+
+void readstr(string & s, const ll len=400005) { char buf[len]; ll a = scanf("%s", buf); a++; s = buf; }
+ll ob(ll i, ll n) { return i < 0 || i >= n; } // out of bounds
+ll tp(ll x) { return (1LL<<x); }
+ll roundup(ll a, ll b) { return a % b ? a/b + 1 : a/b; }
+vector<string> ssplit(string s) { vector<string> ans; stringstream ss; ss << s; while (ss >> s) ans.pb(s); return ans; }
+void makemod(ll & x, ll m) { x %= m; x += m; x %= m; }
+ll powmod(ll a, ll b, ll m) { if (b == 0) return 1;	ll h = powmod(a, b/2, m); ll ans = h*h%m; return b%2 ? ans*a%m : ans; }
+bool isvowel(char ch) { return (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'); }
+ll sign(ll x) {	return x > 0 ? 1 : x < 0 ? -1 : 0; }
+template<typename A> A reversed(const A & a) { A b = a; reverse(all(b)); return b; }
+
+template<typename A, typename B> void upmin(A & x, B v) { x = min(x, (A)v); }
+template<typename A, typename B> void upmax(A & x, B v) { x = max(x, (A)v); }
+
+const VLL mods = {(ll)1e9 + 7, 998244353, (ll)1e6 + 3, (ll)1e18 + 5, 1000000207};
+const ll mod = mods[0];
+const ll inf = mods[3];
+const db eps = 1e-10;
+const db pi = acos(0) * 2;
+const string nl = "\n";
+
+// clang-format on
+
+struct Median {
+    mset<ll> A, B;
+
+    void move(mset<ll>& from, mset<ll>& to, const ll& x) {
+        assert(from.find(x) != from.end());
+        from.erase(from.find(x));
+        to.insert(x);
+    }
+
+    void moveBA() {
+        assert(sz(B));
+        move(B, A, *B.begin());
+    }
+    void moveAB() {
+        assert(sz(A));
+        move(A, B, *A.rbegin());
+    }
+
+    void balance() {
+        while (sz(A) < sz(B)) {
+            moveBA();
+        }
+        while (sz(A) > sz(B) + 1) {
+            moveAB();
+        }
+    }
+
+    void add(ll x) {
+        A.insert(x);
+        moveAB();
+        moveBA();
+        moveAB();
+        moveBA();
+        balance();
+    }
+
+    void remove(ll x) {
+        if (A.find(x) != A.end()) {
+            A.erase(A.find(x));
+        } else if (B.find(x) != B.end()) {
+            B.erase(B.find(x));
+        } else {
+            assert(false);
+        }
+        balance();
+    }
+
+    ll exist(ll x) {
+        return A.find(x) != A.end() || B.find(x) != B.end();
+    }
+    ll size() {
+        return A.size() + B.size();
+    }
+
+    ll twiceQuery() {
+        assert(size() > 0);
+        ll n = sz(A), m = sz(B);
+
+        if (n == m) {
+            ll a = *A.rbegin();
+            ll b = *B.begin();
+            return (a + b);
+        } else if (n == m + 1) {
+            ll a = *A.rbegin();
+            return 2 * a;
+        } else {
+            assert(false);
+        }
+    }
+    db query() {
+        return twiceQuery() / 2.;
+    }
+
+    void print() {
+        if (size() == 0) {
+            printf("Wrong!\n");
+        } else {
+            ll y = twiceQuery();
+
+            if (y % 2) {
+                printf("%.1f\n", y / 2.);
+            } else {
+                printf("%lld\n", y / 2);
+            }
+        }
+    }
+};
+void solve() {
+    // Go crazy
+    Median med;
+
+    ll n;
+    cin >> n;
+    rep(i, 0, n) {
+        string s;
+        ll x;
+        cin >> s >> x;
+        if (s == "r") {
+            if (!med.exist(x)) {
+                printf("Wrong!\n");
+            } else {
+                med.remove(x);
+                med.print();
+            }
+        } else {
+            med.add(x);
+            med.print();
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t = 1;
+    // cin >> t;
+    rep(i, 0, t) solve();
+    return 0;
+}
